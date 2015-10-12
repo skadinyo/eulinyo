d5c0fc22cc63f9a5f2eef4ffe3c624f3a2c6121b
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-10-12 15:52:55 +0700

    120,125

--- src/eulinyo/euler/p120.clj
+++ src/eulinyo/euler/p120.clj
@@ -1,1 +1,24 @@
-(ns eulinyo.euler.p120 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p120
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn r-even
+  [a]
+  (* (- a 2) a))
+
+(defn r-odd
+  [a]
+  (* (- a 1) a))
+
+(defn r-max
+  [a]
+  (if (odd? a)
+    (r-odd a)
+    (r-even a)))
+
+(defn problem-120-a
+  [lim]
+  (->> (range 3 (inc lim))
+       (map r-max)
+       (reduce +)))
--- src/eulinyo/euler/p125.clj
+++ src/eulinyo/euler/p125.clj
@@ -1,1 +1,55 @@
-(ns eulinyo.euler.p125 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p125
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn sum-of-squares
+  [n]
+  (quot (* n (+ n 1) (+ (* 2 n) 1)) 6))
+
+(defn palin?
+  [x]
+  (let [coll (m/number->collection x)]
+    (= coll (reverse coll))))
+
+
+(defn problem-125-a
+  [lim]
+  (let [dada (->> (range 1 (-> lim Math/sqrt int inc))
+                  (map sum-of-squares))]
+    (->> (for [i dada]
+           (->> dada
+                (map #(- % i))
+                (filter (partial < 0))
+                (rest)))
+         (cons (rest dada))
+         (apply concat)
+         (filter (partial > lim))
+         (distinct)
+         (filter palin?)
+         ((juxt count (partial reduce +) sort)))))
+
+(defn problem-125-b
+  [lim]
+  (let [squares (->> (range 1 (-> lim Math/sqrt int inc))
+                     (map sum-of-squares))
+        a-lim (count squares)
+        sum-array (int-array squares)]
+    (->> (loop [c 2
+                [s & ss] squares
+                temp (-> squares rest set)]
+           (if s
+             (recur (inc c)
+                    ss
+                    (let [is (range c a-lim)
+                          ns (->> is
+                                  (map (partial aget sum-array))
+                                  (map #(-' % s))
+                                  (filter (partial > lim)))]
+                      (cse/union temp (set ns))))
+             temp))
+         (filter palin?)
+         (reduce +'))))
+
+;; TODO 125-b, still wrong ?....
+;; TODO Finished, but still to slow.
