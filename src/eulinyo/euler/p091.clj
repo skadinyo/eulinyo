64873b2dfc12551c2d7224410817ca240a4c1d4f
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-10-12 15:52:50 +0700

    unfinished

--- src/eulinyo/euler/p091.clj
+++ src/eulinyo/euler/p091.clj
@@ -1,1 +1,54 @@
-(ns eulinyo.euler.p091 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p091
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]
+            [clojure.math.combinatorics :as combo]))
+
+(defn absolute
+  [x]
+  (if (> x 0)
+    x
+    (* x -1)))
+
+(defn distance
+  [[x1 y1] [x2 y2]]
+  (let [dx (absolute (- x1 x2))
+        dy (absolute (- y1 y2))]
+    (Math/sqrt (+ (m/pow dx 2)
+                  (m/pow dy 2)))))
+
+(def m-distance (memoize distance))
+
+(defn combi-3
+  [p1 p2 p3]
+  [[p1 p2] [p1 p3] [p2 p3]])
+
+(defn phythagoras
+  [points]
+  (let [combi (apply combi-3 points)
+        [a b c] (->> combi
+                     (map (partial apply m-distance))
+                     (sort))]
+    (== c (Math/sqrt (+ (m/pow a 2)
+                        (m/pow b 2))))))
+
+(def m-phytagoras
+  (memoize phythagoras))
+
+(defn problem-91-a
+  [lim]
+  (let [r (range 0 (inc lim))
+        points (for [x r
+                     y r]
+                 [x y])]
+    (->> (for [i1 (rest points)
+               i2 (rest points)
+               :when (not= i1 i2)]
+           [i1 i2])
+         (pmap (partial cons [0 0]))
+         (pmap set)
+         (set)
+         (filter m-phytagoras)
+         (count))))
+
+;; TODO STILL FAILED
--- src/eulinyo/euler/p100.clj
+++ src/eulinyo/euler/p100.clj
@@ -1,1 +1,34 @@
-(ns eulinyo.euler.p100 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p100
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn psquare?
+  [x]
+  (let [root (-> x Math/sqrt)]
+    (== root (bigint root))))
+
+(defn determinant
+  [tot]
+  (inc (*' 2 tot (dec tot))))
+
+(defn blue-disk
+  [tot]
+  (let [det (determinant tot)]
+    (if (psquare? det)
+      (-> det
+          Math/sqrt
+          inc
+          (quot 2))
+      false)))
+
+(defn problem-100-a
+  [start]
+  (loop [i (inc (bigint start))]
+    (do
+      (println i)
+      (let [d (blue-disk i)]
+        (if d [i (bigint d)] (recur (inc i)))))))
+
+
+;; TODO Still wrong
