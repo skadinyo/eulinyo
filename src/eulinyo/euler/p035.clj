0ee61369f654902e0d45f9a4c973c1156e7a1d57
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-10-19 16:09:30 +0700

    problem 35
    130 ms

--- src/eulinyo/euler/p035.clj
+++ src/eulinyo/euler/p035.clj
@@ -1,1 +1,31 @@
-(ns eulinyo.euler.p035 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p035
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]
+            [clojure.core.reducers :as r]))
+
+(defn move-back
+  [[x & xs]]
+  (-> (vec xs)
+      (conj x)))
+
+(defn circular-prime?
+  [x]
+  (if (< x 10)
+    true
+    (let [xs (m/number->collection x)]
+      (if (some #{2 4 5 6 8} xs)
+        false
+        (let [c (count xs)]
+          (->> (iterate move-back xs)
+               (take c)
+               (map m/collection->number)
+               (every? m/prime?)))))))
+
+(defn problem-35-a
+  [lim]
+  (r/reduce (fn [c i]
+              (if (circular-prime? i)
+                (inc c)
+                c))
+            0 (m/prime-sieve lim)))
