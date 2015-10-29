254a153226b347789b752987132da8d0cd793b32
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-10-29 20:21:57 +0700

    problem 123
    solve it like problem 120

--- src/eulinyo/euler/p123.clj
+++ src/eulinyo/euler/p123.clj
@@ -1,1 +1,24 @@
-(ns eulinyo.euler.p123 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p123
+  (:require [eulinyo.math :as m
+             :refer [pow]]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn r
+  [n a]
+  (* 2 a n))
+
+(defn problem-123-a
+  [lim]
+  (let [primes (->> (m/prime-sieve 10000000)
+                    (map vector (rest (range))))]
+    (->> primes
+         (filter #(odd? (first %)))
+         (drop-while #(<= (apply r %) lim))
+         (first)
+         ((juxt identity (partial apply r))))))
+
+(defn r-cal
+  [n a]
+  (rem (+ (pow (dec a) n) (pow (inc a) n))
+       (* a a)))
