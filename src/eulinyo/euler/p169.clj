3300ea312dc4f852bbad70e7a114e09a52799f22
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-24 15:11:36 +0700

    169

--- src/eulinyo/euler/p169.clj
+++ src/eulinyo/euler/p169.clj
@@ -1,1 +1,13 @@
-(ns eulinyo.euler.p169 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p169
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn problem-169-a
+  [n]
+  (if (zero? n)
+    [1 0]
+    (let [[a b] (problem-169-a (quot n 2))]
+      (if (even? n)
+        [(+ a b) b]
+        [a (+ a b)]))))
