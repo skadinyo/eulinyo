85e14342b727577ebdeeacf4fb1618fe0dc7fe70
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-24 15:11:09 +0700

    135,136

--- src/eulinyo/euler/p135.clj
+++ src/eulinyo/euler/p135.clj
@@ -1,1 +1,23 @@
-(ns eulinyo.euler.p135 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p135
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn problem-135-a
+  [lim]
+  (let [temp (int-array (inc lim))]
+    (loop [u 1]
+      (when (< u lim)
+        (loop [v 1]
+          (let [uv (* u v)
+                v3 (* v 3)]
+            (when (<= uv lim)
+              (when (and (> v3 u)
+                         (= 0 (rem (- v3 u) 4))
+                         (= 0 (rem (+ u v) 4)))
+                (aset temp uv (inc (aget temp uv))))
+              (recur (inc v)))))
+        (recur (inc u))))
+    (->> (range 1 lim)
+         (filter #(= 10 (aget temp %)))
+         (count))))
--- src/eulinyo/euler/p136.clj
+++ src/eulinyo/euler/p136.clj
@@ -1,1 +1,23 @@
-(ns eulinyo.euler.p136 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p136
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn problem-136-a
+  [lim]
+  (let [temp (int-array (inc lim))]
+    (loop [u 1]
+      (when (< u lim)
+        (loop [v 1]
+          (let [uv (* u v)
+                v3 (* v 3)]
+            (when (<= uv lim)
+              (when (and (> v3 u)
+                         (= 0 (rem (- v3 u) 4))
+                         (= 0 (rem (+ u v) 4)))
+                (aset temp uv (inc (aget temp uv))))
+              (recur (inc v)))))
+        (recur (inc u))))
+    (->> (range 1 lim)
+         (filter #(= 1 (aget temp %)))
+         (count))))
