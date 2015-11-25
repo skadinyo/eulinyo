56e3b9a099d07ad0c43ecf24eef711856ecc7dda
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-25 19:00:51 +0700

    problem 203

--- src/eulinyo/euler/p203.clj
+++ src/eulinyo/euler/p203.clj
@@ -1,1 +1,18 @@
-(ns eulinyo.euler.p203 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p203
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn put-one
+  [coll]
+  (vec (cons 1 (conj coll 1))))
+
+(defn pascal-tri
+  [n]
+  (loop [c 1 coll [1] res []]
+    (if (> c n)
+      res
+      (let [ncoll (->> (rest coll)
+                       (mapv + coll)
+                       put-one)]
+        (recur (inc c) ncoll (conj res coll))))))
