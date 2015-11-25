ab77bfadfe35f276924b4290575c3969ec66450d
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-25 19:48:21 +0700

    problem 134 brute-force
    6 minutes

--- src/eulinyo/euler/p134.clj
+++ src/eulinyo/euler/p134.clj
@@ -1,1 +1,29 @@
-(ns eulinyo.euler.p134 (:require [eulinyo.core :refer [deft]] [eulinyo.math :as m] [clojure.set :as cse] [clojure.string :as cst]))
+(ns eulinyo.euler.p134
+  (:require [eulinyo.math :as m]
+            [clojure.set :as cse]
+            [clojure.string :as cst]))
+
+(defn find-pair
+  [[[p1 c] p2]]
+  (println p1 p2)
+  (loop [x p2]
+    (let [l (rem x c)]
+      (if (= p1 l)
+        [p1 p2 x]
+        (recur (+ x p2))))))
+
+(defn problem-134
+  []
+  (let [primes (->> (m/prime-sieve 1100000)
+                    (drop-while (partial > 5)))
+        p1s (->> primes
+                 (take-while (partial > 1000000))
+                 (map (fn [x]
+                        [x (m/pow 10 (m/count-number x))])))
+        pairs (map vector p1s (rest primes))]
+    (->> pairs
+         (pmap find-pair)
+         (map last)
+         (reduce +'))))
+
+(comment (time (problem-134)))
